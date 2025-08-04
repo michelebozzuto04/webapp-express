171de7c5bf0fc44c2a28fa42e3c4826b7195f9d2
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-08-04 11:34:31 +0200

    Add public assets folder for image preview. Add cors package.

--- NUOVO/index.js
+++ NUOVO/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const PORT = process.env.PORT;
 const app = express();
 const MovieRouter = require('./routes/MovieRouter');
+const cors = require('cors');
+
+app.use(cors())
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send("Index");
