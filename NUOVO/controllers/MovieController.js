02e211292c8f8c6c6c37aa12703a3c134e6e94fd
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-08-04 11:12:15 +0200

    Add MovieController logic for all movies.

--- NUOVO/controllers/MovieController.js
+++ NUOVO/controllers/MovieController.js
@@ -2,9 +2,19 @@ const connection = require('../database/Connection');
 
 function index(req, res) {
 
-    return (
-        res.send("Ciao")
-    )
+    const sql = "SELECT * FROM movies"
+
+    connection.query(sql, (error, result) => {
+
+        if (error) return (
+            res.status(500).json({
+                error: true,
+                message: error.message
+            })
+        )
+
+        res.json(result);
+    })
 };
 
 function show(req, res) {
